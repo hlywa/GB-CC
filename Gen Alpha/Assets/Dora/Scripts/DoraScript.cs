67392f08c5e5dc88ed69e245216e2806951f59fa
Rachel Heleva <74636622+hlywa@users.noreply.github.com>
2022-08-13 18:39:47 -0700

    Imported Fungus Assets
    lol this doesnt work

---
+++ Gen Alpha/Assets/Dora/Scripts/DoraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoraScript : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start(){
+        animator = this.GetComponent<Animator>();
+    }
+
+    public void standup(){
+        animator.SetBool("sit",false);
+    }
+
+    public void standdown(){
+        animator.SetBool("sit",true);
+        animator.SetBool("happy",false);
+    }
+
+    public void happy(){
+        animator.SetBool("happy",true);
+    }
+}
