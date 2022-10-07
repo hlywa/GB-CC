c162338264a122ed5f1c3b4a7889a826205513d0
Alina Qureshi <qureshi.alina3@gmail.com>
2022-10-08 03:20:38 +0500

    Added 3d models for characters

--- Gen Alpha/Assets/Configs/Characters/CharacterConfig.cs
+++ Gen Alpha/Assets/Configs/Characters/CharacterConfig.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 
 [CreateAssetMenu(fileName = "Character", menuName = "Desktop/Forum/Character", order = 1)]
 public class CharacterConfig : ScriptableObject
 {
-    public eGamerTag m_gamerTag;
-    public string m_gamerTagText;
-    public Sprite m_profilePicture;
-    public string m_realName;
-    public string m_description;
+    [FormerlySerializedAs("m_gamerTag")] public eGamerTag GamerTag;
+    [FormerlySerializedAs("m_gamerTagText")] public string GamerTagText;
+    [FormerlySerializedAs("m_profilePicture")] public Sprite ProfilePicture;
+    [FormerlySerializedAs("m_realName")] public string RealName;
+    [FormerlySerializedAs("m_description")] public string Description;
 }
